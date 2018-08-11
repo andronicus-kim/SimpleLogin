06505358ed397f8eed6cdcf6fad7be9a1413dfd6
andronicus-kim <androkim.kim7@gmail.com>
2018-08-11 14:53:10 +0300

    saves user to db

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+
 const router = express()
 
 router.post('/login',(req,res) => res.send(req.body))
--- routes/signup.js
+++ routes/signup.js
@@ -3,6 +3,22 @@ const collections = require('../db/db')
 
 const router = express()
 
-router.post('/signup',(req,res) => collections.users.insert(req.body))
+router.post('/signup', async (req, res) => {
+    try {
+        let response = await collections.users.insert(req.body)
+        throw {
+            error: 'some self error',
+            statusCode: 305
+        }
+        
+        response.result.ok ? res.status(200).send(response) : res.status(400).send(response)
+
+    } catch (e) {
+        res.status(e.statusCode || 500).send({
+            error: e.error || e.toString()
+        })
+    }
+
+})
 
 module.exports = router
